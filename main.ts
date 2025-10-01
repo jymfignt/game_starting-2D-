40a28724801abc41e765a97ea5385439f3c2ef17
jymfignt <147328457+jymfignt@users.noreply.github.com>
2025-10-01 10:20:52 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,5 +2,10 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+app.use(router)
+app.use(ElementPlus)   // 挂载 Element Plus
+app.mount('#app')
